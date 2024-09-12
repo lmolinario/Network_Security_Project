5c7bf054b0346fa475ec8310b7720ed5824890de
Lello Molinario <l.molinario@studenti.unica.it>
2024-09-12 10:27:23 +0200

    Generate a "logo" and set text colours

---
+++ utils/logo.py
@@ -0,0 +1,38 @@
+# A class for text colours is defined
+import time
+
+v = '\u001b[92m' #GREEN
+g = '\033[93m' #YELLOW
+r = '\033[91m' #RED
+b = '\033[34m' #BLU
+reset = '\033[0m' #RESET
+
+
+print(v + "\t\t   _   _      _                      _")
+print("\t\t  | \\ | |    | |                    | |")
+print("\t\t  |  \\| | ___| |___      _____  _ __| | __")
+print("\t\t  | . ` |/ _ \\ __\\ \\ /\\ / / _ \\| '__| |/ /")
+print("\t\t  | |\\  |  __/ |_ \\ V  V / (_) | |  |   <")
+print("\t\t  |_| \\_|\\___|\\__| \\_/\\_/ \\___/|_|  |_|\\_\\")
+print("\t\t   / ____|                    (_) |")
+print("\t\t  | (___   ___  ___ _   _ _ __ _| |_ _   _")
+print("\t\t   \\___ \\ / _ \\/ __| | | | '__| | __| | | |")
+print("\t\t   ____) |  __/ (__| |_| | |  | | |_| |_| |")
+print("\t\t  |_____/ \\___|\\___|\\__,_|_|  |_|\\__|\\__, |")
+print("\t\t  |  __ \\         (_)         | |     __/ |")
+print("\t\t  | |__) | __ ___  _  ___  ___| |_   |___/")
+print("\t\t  |  ___/ '__/ _ \\| |/ _ \\/ __| __|")
+print("\t\t  | |   | | | (_) | |  __/ (__| |_")
+print("\t\t  |_|   |_|  \\___/| |\\___|\\___|\\__|")
+print("\t\t                 _/ |")
+print("\t\t                |__/                       " + reset)
+
+print(
+    r + "\nAuthors:\nLello Molinario (matr.70/90/00369)\nFederico Moro (matr.70/90/00380)" + reset)
+
+
+print("Master's degree in Computer Engineering, Cybersecurity and Artificial Intelligence - University of Cagliari")
+
+
+print(b + "Supervisor: Prof. Marco Martalò\n" + reset)
+time.sleep(2)
