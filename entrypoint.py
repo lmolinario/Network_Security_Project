024c81d88fe9b6ed838691fac9d656d929710d56
Federico0307 <76046251+Federico0307@users.noreply.github.com>
2024-09-13 10:48:59 +0200

    Update entrypoint.py
    added the call to the function 'hack_wifi'

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,6 @@
 from  utils.gps_manager import start_gps
 from  utils.wifi_monitor import scan_wifi
-
+from utils.vulnerability_finder import vuln_find
 
 
 def main():
@@ -11,6 +11,8 @@ def main():
     #     Start wifi scan via less powerful wlan0 (raspberry's network card) to avoid taking other networks except our
     wifi_list = scan_wifi()
 
+    #     Hack of wifi via wlan1 (monitor network card  "Alfa" )
+    wifi_list_exploited = hack_wifi(wifi_list)
 
 
 
