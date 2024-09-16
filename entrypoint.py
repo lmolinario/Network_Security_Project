acfe0c069aef392ab76243e7121cf7192dcd27cb
Lello <l.molinario@studenti.unica.it>
2024-09-16 15:30:33 +0200

    update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,6 @@
 from  utils.gps_manager import start_gps
 from  utils.wifi_monitor import scan_wifi
+from utils.wifi_exploit import hack_wifi
 from utils.vulnerability_finder import vuln_find
 
 
@@ -14,6 +15,8 @@ def main():
     #     Hack of wifi via wlan1 (monitor network card  "Alfa" )
     wifi_list_exploited = hack_wifi(wifi_list)
 
+    #     Vulnerabilities scan
+    wifi_list_mapped = vuln_find(wifi_list_exploited)
 
 
 if __name__ == "__main__":
